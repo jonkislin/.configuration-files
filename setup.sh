752966dfaeda1a8d99ef02b7de5d51e95509cf05
jonkislin <jon.kislin@gmail.com>
2020-10-28 12:10:27 -0400

    jon edits, will test on digital ocean

--- setup.sh
+++ setup.sh
@@ -6,9 +6,13 @@ cd $HOME/.opineconfig
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install custom zsh theme: lambda-gitster
+git clone https://github.com/ergenekonyigit/lambda-gitster.git
+cd lambda-gitster
+cp lambda-gitster.zsh-theme ~/.oh-my-zsh/custom/themes
+
 # Symlink everything
 ln -sf ./.vim ~/.vim
-ln -sf ./.tmux.conf ~/.tmux.conf
 ln -sf ./.zshrc ~/.zshrc
 ln -sf ./.dir_colors ~/.dir_colors
 ln -sf ./.aliases ~/.aliases
@@ -16,3 +20,5 @@ ln -sf ./.aliases ~/.aliases
 # Setup vim
 vim +PlugInstall +qa
 
+# Source zsh config, which handles all else
+source ~/.zshrc
